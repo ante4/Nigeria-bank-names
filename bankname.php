c19e65b3112c3382937ee412e2987b5f5d04cb38
Ante Esang <31421747+ante4@users.noreply.github.com>
2021-01-12 01:43:56 +0100

    This is a list of banks in Nigeria.

---
+++ bankname.php
@@ -0,0 +1,83 @@
+DB::table('banknames')->insert([
+            [
+                'name' => 'Access Bank Plc',
+            ],
+            [
+                'name' => 'Citibank Nigeria Ltd',
+            ],
+            [
+                'name' => 'Dynamic Standard Bank',
+            ],
+            [
+                'name' => 'Diamond Bank Plc',
+            ],
+            [
+                'name' => 'Ecobank Nigeria Plc',
+            ],
+            [
+                'name' => 'Fidelity Bank Plc',
+            ],
+            [
+                'name' => 'First City Monument Bank Plc',
+            ],
+            [
+                'name' => 'First Bank Nigeria Ltd',
+            ],
+            [
+                'name' => 'Guaranty Trust Bank Plc',
+            ],
+            [
+                'name' => 'Globus Bank Ltd',
+            ],
+            [
+                'name' => 'Heritage Banking Company Ltd',
+            ],
+            [
+                'name' => 'Jaiz Bank Plc',
+            ],
+            [
+                'name' => 'Keystone Bank Ltd',
+            ],
+            [
+                'name' => 'Kuda Microfinance Bank',
+            ],
+            [
+                'name' => 'Polaris Bank Ltd',
+            ],
+            [
+                'name' => 'Providus Bank Ltd',
+            ],
+            [
+                'name' => 'Stanbic IBTC Bank Plc',
+            ],
+            [
+                'name' => 'Standard Chartered Bank Nigeria Ltd',
+            ],
+            [
+                'name' => 'Sterling Bank Plc',
+            ],
+            [
+                'name' => 'SunTrust Bank Nigeria Ltd',
+            ],
+            [
+                'name' => 'Titan Trust Bank Ltd',
+            ],
+            [
+                'name' => 'TAJ Bank Ltd',
+            ], 
+            [
+                'name' => 'Union Bank of Nigeria Plc',
+            ],
+            [
+                'name' => 'United Bank For Africa Plc',
+            ],
+            [
+                'name' => 'Unity Bank Plc',
+            ],
+            [
+                'name' => 'Wema Bank Plc',
+            ],
+            [
+                'name' => 'Zenith Bank Plc',
+            ],
+        ]);
